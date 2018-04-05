414cb7dfec2e239e0137209de7dfb7678550f1fa
Jorge Yanez <ofeth@hotmail.com>
2018-04-05 18:37:28 -0500

    Fixed spaces

--- BitLogger.js
+++ BitLogger.js
@@ -14,8 +14,8 @@ var bitLogger = bitLogger || (function bitLoggerModule() {
     /**
      * Flags to indicate what is shown in the final message
      */
-	const show = {
-		date: true,
+    const show = {
+	date: true,
         time: true,
         level: true
     };
@@ -23,44 +23,44 @@ var bitLogger = bitLogger || (function bitLoggerModule() {
     /**
      * Gets the date and time in ISO format
      */
-	function getDateTime() {
-		let currentDate = new Date();
-		let dateTime = currentDate
-			.toISOString()
-			.replace(/([T|Z]+)/g, whitespace)
-			.replace(/([.][0-9]{1,3})/, empty)
-			.trim()
-			.split(whitespace);
+    function getDateTime() {
+        let currentDate = new Date();
+        let dateTime = currentDate
+                .toISOString()
+                .replace(/([T|Z]+)/g, whitespace)
+                .replace(/([.][0-9]{1,3})/, empty)
+                .trim()
+                .split(whitespace);
 
-		if (show.date && show.time) {
-			return dateTime.join(whitespace);
-		} else if (show.date) {
-			return dateTime[0];
-		} else if (show.time) {
-			return dateTime[1];
-		}
-	}
+        if (show.date && show.time) {
+                return dateTime.join(whitespace);
+        } else if (show.date) {
+                return dateTime[0];
+        } else if (show.time) {
+                return dateTime[1];
+        }
+    }
 
     /**
      * Writes a message to the console
      * @param {*} level level of the message
      * @param {*} message message to be written
      */
-	function write(level, message) {
-		if (!level) {
-			throw new TypeError('Level must be defined');
-		}
-		
-		if (!message) {
-			throw new TypeError('message must be defined');
-		}
-
-		if (typeof (message) !== 'string') {
-			message = JSON.stringify(message);
+    function write(level, message) {
+        if (!level) {
+            throw new TypeError('Level must be defined');
         }
-        
+    
+        if (!message) {
+            throw new TypeError('message must be defined');
+        }
+
+        if (typeof (message) !== 'string') {
+            message = JSON.stringify(message);
+        }
+
         console.log(`${(show.date || show.time ? `${getDateTime()} | ` : empty)}${(show.level ? `<${level}> - ` : empty)}${message}`);
-	}
+    }
 
     /**
      * Writes a messages as critical
@@ -96,18 +96,18 @@ var bitLogger = bitLogger || (function bitLoggerModule() {
      * Writes a messages as debug
      * @param {*} message text or object to be logged
      */
-	const debug = (message) => write('debug', message);
+    const debug = (message) => write('debug', message);
 
     /**
      * Set of functionality exposed outside the module
      */
     const API = {
-		critical: critical,
-		error: error,
-		warning: warning,
-		information: information,
-		verbose: verbose,
-		debug: debug
-	};
-	return API;
+        critical: critical,
+        error: error,
+        warning: warning,
+        information: information,
+        verbose: verbose,
+        debug: debug
+    };
+    return API;
 })();
