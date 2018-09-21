5fd307cddc2a3837d30c343c6a96c5cc1020115b
Jorge Yañez <ofeth@hotmail.com>
2018-09-21 14:26:14 -0600

    Update BitLogger.js
    updated syntax to use ES6 classes

--- BitLogger.js
+++ BitLogger.js
@@ -1,113 +1,107 @@
-var bitLogger = bitLogger || (function bitLoggerModule() {
-    'use strict';
-    
-    /**
-     * Represents a whitespace
-     */
-    const whitespace = ' ';
-    
-    /**
-     * Represents an empty string
-     */
-    const empty = '';
-    
-    /**
-     * Flags to indicate what is shown in the final message
-     */
-    const show = {
-	date: true,
-        time: true,
-        level: true
+class bitLogger {
+  constructor(config) {
+    this.levels = {
+      critical: {
+        name: "critical",
+        value: 1
+      },
+      error: {
+        name: "error",
+        value: 2
+      },
+      warning: {
+        name: "warning",
+        value: 3
+      },
+      information: {
+        name: "information",
+        value: 4
+      },
+      verbose: {
+        name: "verbose",
+        value: 5
+      },
+      debug: {
+        name: "debug",
+        value: 6
+      }
     };
-    
-    /**
-     * Gets the date and time in ISO format
-     */
-    function getDateTime() {
-        let currentDate = new Date();
-        let dateTime = currentDate
-                .toISOString()
-                .replace(/([T|Z]+)/g, whitespace)
-                .replace(/([.][0-9]{1,3})/, empty)
-                .trim()
-                .split(whitespace);
 
-        if (show.date && show.time) {
-            return dateTime.join(whitespace);
-        } else if (show.date) {
-            return dateTime[0];
-        } else if (show.time) {
-            return dateTime[1];
-        }
+    this.config = config || {
+      showDate: true,
+      showTime: true,
+      showLevel: true,
+      level: this.levels.debug
+    };
+
+    this.whitespace = " ";
+    this.empty = "";
+  }
+
+  getDateTime() {
+    let currentDate = new Date();
+    let dateTime = currentDate
+      .toISOString()
+      .replace(/([T|Z]+)/g, this.whitespace)
+      .replace(/([.][0-9]{1,3})/, this.empty)
+      .trim()
+      .split(this.whitespace);
+
+    if (this.config.showDate && this.config.showTime) {
+      return dateTime.join(this.whitespace);
+    } else if (this.config.showDate) {
+      return dateTime[0];
+    } else if (this.config.showTime) {
+      return dateTime[1];
+    }
+  }
+
+  write(level, message) {
+    if (!level) {
+      throw new TypeError("Level must be defined");
     }
 
-    /**
-     * Writes a message to the console
-     * @param {*} level level of the message
-     * @param {*} message message to be written
-     */
-    function write(level, message) {
-        if (!level) {
-            throw new TypeError('Level must be defined');
-        }
-    
-        if (!message) {
-            throw new TypeError('message must be defined');
-        }
-
-        if (typeof (message) !== 'string') {
-            message = JSON.stringify(message);
-        }
-
-        console.log(`${(show.date || show.time ? `${getDateTime()} | ` : empty)}${(show.level ? `<${level}> - ` : empty)}${message}`);
+    if (!message) {
+      throw new TypeError("message must be defined");
     }
 
-    /**
-     * Writes a messages as critical
-     * @param {*} message text or object to be logged
-     */
-    const critical = (message) => write('critical', message);
-    
-    /**
-     * Writes a messages as error
-     * @param {*} message text or object to be logged
-     */
-    const error = (message) => write('error', message);
-    
-    /**
-     * Writes a messages as warning
-     * @param {*} message text or object to be logged
-     */
-    const warning = (message) => write('warning', message);
-    
-    /**
-     * Writes a messages as information
-     * @param {*} message text or object to be logged
-     */
-    const information = (message) => write('information', message);
-    
-    /**
-     * Writes a messages as verbose
-     * @param {*} message text or object to be logged
-     */
-    const verbose = (message) => write('verbose', message);
-    
-    /**
-     * Writes a messages as debug
-     * @param {*} message text or object to be logged
-     */
-    const debug = (message) => write('debug', message);
+    if (typeof message !== "string") {
+      message = JSON.stringify(message);
+    }
 
-    /**
-     * Set of functionality exposed outside the module
-     */
-    const API = {
-        critical: critical,
-        error: error,
-        warning: warning,
-        information: information,
-        verbose: verbose,
-        debug: debug
-    };
-    return API;
-})();
+    let shouldDisplayDateOrTime = this.config.showDate || this.config.showTime;
+    let shouldShowLevel = this.config.showLevel;
+    let displayDatetime = shouldDisplayDateOrTime
+      ? `${this.getDateTime()} | `
+      : this.empty;
+    let displayLevel = shouldShowLevel ? `<${level.name}> - ` : this.empty;
+    let writeMessage = this.config.level.value >= level.value;
+
+    if (writeMessage)
+      console.log(`${displayDatetime}${displayLevel}${message}`);
+  }
+
+  critical(message) {
+    this.write(this.levels.critical, message);
+  }
+
+  error(message) {
+    this.write(this.levels.error, message);
+  }
+
+  warning(message) {
+    this.write(this.levels.warning, message);
+  }
+
+  information(message) {
+    this.write(this.levels.information, message);
+  }
+
+  verbose(message) {
+    this.write(this.levels.verbose, message);
+  }
+
+  debug(message) {
+    this.write(this.levels.debug, message);
+  }
+}
